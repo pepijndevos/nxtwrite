e3b99d7387427e558b6ac109d062a29623a1114b
Pepijn de Vos <pepijndevos@gmail.com>
2011-12-12 14:18:07 +0100

    using motcont

--- nxtdemo.py
+++ nxtdemo.py
@@ -1,8 +1,5 @@
 from nxt import locator, motor
 import nxtprinter
 b = locator.find_one_brick()
-ym = motor.Motor(b, motor.PORT_A)
-xm = motor.Motor(b, motor.PORT_B)
-pm = motor.Motor(b, motor.PORT_C)
-p = nxtprinter.NxtPrinter(xm, ym, pm)
+p = nxtprinter.NxtPrinter(b, motor.PORT_B, motor.PORT_A, motor.PORT_C)
 p.write(raw_input("print: "), 15)
--- nxtprinter.py
+++ nxtprinter.py
@@ -1,5 +1,5 @@
 from baseprinter import BasePrinter
-from nxt import locator, motor
+from nxt import locator, motor, motcont
 from multiprocessing.pool import ThreadPool
 from time import sleep
 
@@ -8,12 +8,17 @@ class NxtPrinter(BasePrinter):
     xscale = 10
     yscale = -20
 
-    power = 20
+    power = 50
 
-    def __init__(self, xmotor, ymotor, penmotor):
+    def __init__(self, brick, xmotor, ymotor, penmotor):
+        self.brick = brick
         self.xmotor = xmotor
         self.ymotor = ymotor
         self.penmotor = penmotor
+
+        mc = motcont.MotCont(brick)
+        mc.start()
+        self.motcont = mc
         
         self.calib()
 
@@ -25,31 +30,27 @@ class NxtPrinter(BasePrinter):
         self.move = submit
    
     def calib(self):
-        self.penmotor.run(-60)
+        m = motor.Motor(self.brick, self.penmotor)
+        m.run(-60)
         sleep(1)
-        self.penmotor.idle()
-	self.downtacho = self.penmotor.get_tacho().tacho_count
+        m.idle()
+	self.downtacho = m.get_tacho().tacho_count
         self.up()
 
     def turn(self, args):
         motor = args[0]
         steps = args[1]
-        if steps != 0:
-            # make steps positive, put the sign on power
-            power = self.power * cmp(steps, 0)
-            motor.turn(power, abs(steps))
+        self.motcont.move_to(motor, self.power, steps)
+        while(not self.motcont.is_ready(motor)):
+            pass
 
     def up(self):
-        steps = 0 - self.penmotor.get_tacho().tacho_count
-        self.turn([self.penmotor, steps])
+        self.turn([self.penmotor, 0])
 
     def down(self):
-        steps = self.downtacho - self.penmotor.get_tacho().tacho_count
-        self.turn([self.penmotor, steps])
+        self.turn([self.penmotor, self.downtacho])
 
     def _to(self, x, y):
-        xtacho = self.xmotor.get_tacho().tacho_count
-        ytacho = self.ymotor.get_tacho().tacho_count
-        vertial = (self.ymotor, y - ytacho)
-        horizontal = (self.xmotor, x - xtacho)
+        vertial = (self.ymotor, y)
+        horizontal = (self.xmotor, x)
         self.move(vertial, horizontal)
